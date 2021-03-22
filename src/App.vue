5cad68b4a8f7c68df802e52f7de8fa7519e232ec
blacktunes <blacktunes@foxmail.com>
2021-03-22 18:00:23 +0800

    feat: 重做页面效果

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,158 @@
 <template>
-  <div class="loading" v-show="isShow && !isLoad">
-    <div>loading</div>
-  </div>
-  <div class="index" @click="show">
-    <img @load="load" v-if="isShow" alt="hiiro" src="/Hiiro.jpg" />
+  <div class="index"></div>
+  <template v-if="isShow">
+    <transition name="fade" appear>
+      <div class="card-list">
+        <Card v-for="(card, key) in Data" :key="'card' + key" class="card">
+          {{ card.text }}
+          <template #name>{{ card.name }}</template>
+        </Card>
+      </div>
+    </transition>
+  </template>
+  <div class="list" v-else @click.once="showAll">
+    <template v-for="(item, index) in showList" :key="index">
+      <div class="random-card" :style="{ opacity: item.show ? 1 : 0 }">
+        <Card style="position: fixed" :style="{ top: item.y, left: item.x }">
+          {{ item.text }}
+          <template #name>{{ item.name }}</template>
+        </Card>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { onDeactivated, onMounted, ref } from 'vue'
+import Card from './components/Card.vue'
+import Data from './assets/hrk.json'
 
 export default {
+  components: { Card },
   setup() {
     const isShow = ref(false)
-    const show = () => {
-      isShow.value = !isShow.value
+    const showAll = () => {
+      isShow.value = true
+      remove()
     }
 
-    const isLoad = ref(false)
-    const load = () => {
-      isLoad.value = true
+    const random = (min, max) => {
+      if (min === undefined || max === undefined) {
+        return Math.random()
+      }
+      return Math.floor(Math.random() * (Number(max) - Number(min) + 1)) + Number(min)
     }
+
+    // 需要生成的卡片数
+    const num = 10
+    // 随机区间
+    const r = [2000, 8000]
+
+    const showList = ref([])
+    for (let i = 0; i < num; i++) {
+      const data = Data[random(0, Data.length - 1)]
+      showList.value[i] = {
+        show: false,
+        x: random(0, 80) + 'vw',
+        y: random(0, 80) + 'vh',
+        name: data.name,
+        text: data.text
+      }
+    }
+
+    let timers = {}
+    const remove = () => {
+      for (const i in timers) {
+        clearTimeout(timers[i])
+        timers[i] = null
+      }
+      timers = null
+    }
+
+    const testTimer = (key) => {
+      if (timers) {
+        const time = Date.now()
+        timers[time] = setTimeout(() => {
+          showList.value[key].show = !showList.value[key].show
+          if (showList.value[key].show) {
+            showList.value[key].x = random(0, 80) + 'vw'
+            showList.value[key].y = random(0, 80) + 'vh'
+          }
+          if (timers) {
+            delete timers[time]
+          }
+          testTimer(key)
+        }, random(r[0], r[1]))
+      }
+    }
+
+    onMounted(() => {
+      for (let i = 0; i < num; i++) {
+        testTimer(i)
+      }
+    })
+
+    onDeactivated(() => {
+      remove()
+    })
 
     return {
       isShow,
-      show,
-      isLoad,
-      load
+      showAll,
+      Data,
+      showList,
+      random
     }
   }
 }
 </script>
 
 <style lang="stylus">
+.index
+  z-index -1
+  position fixed
+  top 0
+  left 0
+  height 100vh
+  width 100vw
+  background url('/bg.jpg') center center no-repeat
+  background-size contain
+
+.card-list
+  z-index 2
+  display flex
+  flex-direction column
+  align-items center
+
+  .card
+    max-width 80%
+
+.list
+  z-index 1
+  overflow hidden
+  position fixed
+  top 0
+  left 0
+  height 100vh
+  width 100vw
+
+  .random-card
+    opacity 0
+    transition opacity 0.3s
+
+@media only screen and (min-width 900px)
+  .card
+    max-width 600px !important
+
+.fade-enter-active, .fade-leave-active
+  transition opacity 0.3s
+
+.fade-enter-from, .fade-leave-to
+  opacity 0
+
+.fade-enter-to, .fade-leave-from
+  opacity 1
+
 body
   margin 0
   -webkit-tap-highlight-color rgba(0, 0, 0, 0)
@@ -53,24 +173,4 @@ body
 
 ::-webkit-scrollbar-thumb:active
   background-color #FADBD8
-
-.index
-  height 100vh
-  width 100vw
-  background url('/bg.jpg') center center no-repeat
-  background-size contain
-
-.loading
-  position fixed
-  top 0
-  left 0
-  height 100vh
-  width 100vw
-  background rgba(0, 0, 0, 0.5)
-
-  div
-    position fixed
-    top 50%
-    left 50%
-    transform translate(-50%, -50%)
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card-wrapper">
+    <div class="card-text">
+      <slot></slot>
+    </div>
+    <div class="card-name" v-if="$slots.name">——<slot name="name"></slot></div>
+  </div>
+</template>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.card-wrapper
+  display inline-block
+  min-width 200px
+  max-width 300px
+  margin 10px 5px
+  padding 10px
+  border-radius 5px
+  border 1px solid #F5C1BB
+  background-color #FADBD899
+  color #303133
+  transition box-shadow 0.25s, background-color 0.2s
+  user-select none
+
+  &:hover
+    z-index 9
+    background-color #FADBD8 !important
+    box-shadow 2px 2px 5px rgba(0, 0, 0, 0.1)
+
+  .card-text
+    text-align center
+    padding 10px
+
+  .card-name
+    text-align right
+    padding 10px
+</style>
