5cd35208050fdf1fe364c25751e5667d73b3fb38
blacktunes <blacktunes@foxmail.com>
2021-03-23 17:51:06 +0800

    feat: 重做页面效果

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <div class="index"></div>
-  <template v-if="isShow">
+  <Select ref="select">
+    <div @click="reset" v-if="isRandom || isShowAll" class="icon">Select</div>
+    <div @click="setRandom" v-if="!isRandom" class="icon">Random</div>
+    <div @click="showAll" v-if="!isShowAll" class="icon">All</div>
+  </Select>
+  <div class="home" :class="[isHorizontal ? 'horizontal' : 'vertical']">
+    <div>
+      <div
+        v-if="!isRandom && !isShowAll"
+        class="grids"
+        :style="{
+          width: isHorizontal ? 100 * (993 / 1105) + 'vh' : '100vw',
+          height: isHorizontal ? '100vh' : 100 * (1105 / 993) + 'vw',
+        }"
+      >
+        <div
+          class="grid"
+          v-for="i in 4988"
+          :key="i"
+          @mouseenter="show(i)"
+        ></div>
+      </div>
+    </div>
+  </div>
+  <div
+    class="card"
+    :style="{ opacity: isShow ? 1 : 0 }"
+    v-if="!isRandom && !isShowAll"
+  >
+    <Card class="mid-card">
+      {{ Data[selectIndex].text }}
+      <template #name>{{ Data[selectIndex].name }}</template>
+    </Card>
+  </div>
+  <template v-else-if="isShowAll">
     <transition name="fade" appear>
-      <div class="card-list">
-        <Card v-for="(card, key) in Data" :key="'card' + key" class="card">
+      <div class="all-list">
+        <Card v-for="(card, key) in Data" :key="'card' + key" class="all-card">
           {{ card.text }}
           <template #name>{{ card.name }}</template>
         </Card>
       </div>
     </transition>
   </template>
-  <div class="list" v-else @click.once="showAll">
-    <template v-for="(item, index) in showList" :key="index">
+  <div
+    class="random-list"
+    :style="{ 'z-index': isRandom ? 1 : -1 }"
+    v-else-if="isRandom"
+  >
+    <template v-for="(item, key) in showList" :key="'random' + key">
       <div class="random-card" :style="{ opacity: item.show ? 1 : 0 }">
         <Card style="position: fixed" :style="{ top: item.y, left: item.x }">
           {{ item.text }}
@@ -23,92 +60,179 @@
 </template>
 
 <script>
-import { onDeactivated, onMounted, ref } from 'vue'
-import Card from './components/Card.vue'
+import { onMounted, ref } from 'vue'
 import Data from './assets/hrk.json'
+import Card from './components/Card.vue'
+import Select from './components/Select.vue'
+
+const initDefault = () => {
+  /** 是否显示选择卡片 */
+  const isShow = ref(false)
+  const selectIndex = ref(0)
+
+  const getIndex = (key) => {
+    let temp = key
+    if (temp > (Data.length - 1)) {
+      temp -= (Data.length - 1)
+      return getIndex(temp)
+    } else {
+      return temp
+    }
+  }
+
+  const show = (key) => {
+    isShow.value = true
+    selectIndex.value = getIndex(key - 1)
+  }
+
+  return {
+    selectIndex,
+    isShow,
+    show
+  }
+}
+
+/**
+ * @param {number} num  生成的卡片数
+ * @param {[number, number]} r 随机区间
+ */
+const initRandom = (num, r) => {
+  const isRandom = ref(false)
+
+  const random = (min, max) => {
+    if (min === undefined || max === undefined) {
+      return Math.random()
+    }
+    return Math.floor(Math.random() * (Number(max) - Number(min) + 1)) + Number(min)
+  }
+
+  const showList = ref([])
+  for (let i = 0; i < num; i++) {
+    const data = Data[random(0, Data.length - 1)]
+    showList.value[i] = {
+      show: false,
+      x: random(0, 80) + 'vw',
+      y: random(0, 80) + 'vh',
+      name: data.name,
+      text: data.text
+    }
+  }
+
+  let timers = {}
+  const remove = () => {
+    for (const i in timers) {
+      clearTimeout(timers[i])
+      timers[i] = null
+    }
+    for (const item of showList.value) {
+      item.show = false
+    }
+    timers = {}
+  }
+
+  const setTimer = (key, times) => {
+    if (timers) {
+      const time = Date.now()
+      timers[time] = setTimeout(() => {
+        showList.value[key].show = !showList.value[key].show
+        if (showList.value[key].show) {
+          showList.value[key].x = random(0, 80) + 'vw'
+          showList.value[key].y = random(0, 80) + 'vh'
+        }
+        if (timers) {
+          delete timers[time]
+        }
+        setTimer(key)
+      }, random(times ? times[0] : r[0], times ? times[1] : r[1]))
+    }
+  }
+
+  const initCard = () => {
+    for (let i = 0; i < num; i++) {
+      setTimer(i, [500, 1000])
+    }
+  }
+
+  return {
+    isRandom,
+    showList,
+    initCard,
+    remove
+  }
+}
 
 export default {
-  components: { Card },
+  components: {
+    Card,
+    Select
+  },
   setup() {
-    const isShow = ref(false)
-    const showAll = () => {
-      isShow.value = true
-      remove()
+    const select = ref()
+
+    const isHorizontal = ref(true)
+    const getDirection = () => {
+      isHorizontal.value = document.documentElement.clientWidth > document.documentElement.clientHeight
     }
-
-    const random = (min, max) => {
-      if (min === undefined || max === undefined) {
-        return Math.random()
-      }
-      return Math.floor(Math.random() * (Number(max) - Number(min) + 1)) + Number(min)
-    }
-
-    // 需要生成的卡片数
-    const num = 10
-    // 随机区间
-    const r = [2000, 8000]
-
-    const showList = ref([])
-    for (let i = 0; i < num; i++) {
-      const data = Data[random(0, Data.length - 1)]
-      showList.value[i] = {
-        show: false,
-        x: random(0, 80) + 'vw',
-        y: random(0, 80) + 'vh',
-        name: data.name,
-        text: data.text
-      }
-    }
-
-    let timers = {}
-    const remove = () => {
-      for (const i in timers) {
-        clearTimeout(timers[i])
-        timers[i] = null
-      }
-      timers = null
-    }
-
-    const testTimer = (key) => {
-      if (timers) {
-        const time = Date.now()
-        timers[time] = setTimeout(() => {
-          showList.value[key].show = !showList.value[key].show
-          if (showList.value[key].show) {
-            showList.value[key].x = random(0, 80) + 'vw'
-            showList.value[key].y = random(0, 80) + 'vh'
-          }
-          if (timers) {
-            delete timers[time]
-          }
-          testTimer(key)
-        }, random(r[0], r[1]))
-      }
-    }
-
     onMounted(() => {
-      for (let i = 0; i < num; i++) {
-        testTimer(i)
-      }
+      window.onresize = getDirection
     })
 
-    onDeactivated(() => {
+    const { selectIndex, isShow, show } = initDefault()
+    const { isRandom, showList, initCard, remove } = initRandom(10, [2000, 8000])
+
+    const reset = () => {
+      isShow.value = false
+      isRandom.value = false
       remove()
-    })
+      isShowAll.value = false
+      select.value.showItem()
+    }
+
+    const setRandom = () => {
+      isShowAll.value = false
+      isRandom.value = true
+      initCard()
+      select.value.showItem()
+    }
+
+    const isShowAll = ref(false)
+    const showAll = () => {
+      isRandom.value = false
+      remove()
+      isShowAll.value = true
+      select.value.showItem()
+    }
+
+
 
     return {
-      isShow,
-      showAll,
+      select,
       Data,
-      showList,
-      random
+      isHorizontal,
+      selectIndex, isShow, show,
+      isRandom, setRandom, showList, remove,
+      isShowAll,
+      showAll,
+      reset
     }
   }
 }
 </script>
 
 <style lang="stylus">
-.index
+body
+  margin 0
+
+.icon
+  height 40px
+  line-height 40px
+  user-select none
+  cursor pointer
+  text-align center
+  font-size 12px
+  font-weight bold
+
+.home
   z-index -1
   position fixed
   top 0
@@ -118,16 +242,39 @@ export default {
   background url('/bg.jpg') center center no-repeat
   background-size contain
 
-.card-list
+.grids
+  display grid
+  grid-template-columns repeat(58, 1.724%)
+  grid-template-rows repeat(86, 1.163%)
+  grid-row-gap 0px
+  grid-column-gap 0px
+  position fixed
+  top 50%
+  left 50%
+  transform translate(-50%, -50%)
+
+  .grid:hover
+    box-sizing border-box
+    border 1px solid red
+
+.card
+  position fixed
+  top 50%
+  left 50%
+  transform translate(-50%, -50%)
+  pointer-events none
+  transition opacity 0.3s
+
+.all-list
   z-index 2
   display flex
   flex-direction column
   align-items center
 
-  .card
+  .all-card
     max-width 80%
 
-.list
+.random-list
   z-index 1
   overflow hidden
   position fixed
@@ -140,8 +287,28 @@ export default {
     opacity 0
     transition opacity 0.3s
 
+.horizontal
+  flex-direction column
+
+  .img
+    height 100vh
+
+.vertical
+  flex-direction row
+
+  .img
+    width 100vw
+
 @media only screen and (min-width 900px)
-  .card
+  .mid-card
+    max-width 600px !important
+
+@media only screen and (max-width 500px)
+  .random-card
+    font-size 10px !important
+
+@media only screen and (min-width 900px)
+  .all-card
     max-width 600px !important
 
 .fade-enter-active, .fade-leave-active
@@ -152,25 +319,4 @@ export default {
 
 .fade-enter-to, .fade-leave-from
   opacity 1
-
-body
-  margin 0
-  -webkit-tap-highlight-color rgba(0, 0, 0, 0)
-
-::-webkit-scrollbar
-  width 7px
-  height 7px
-
-::-webkit-scrollbar-track
-  box-shadow inset 0 0 6px rgba(0, 0, 0, 0.3)
-  -webkit-box-shadow inset 0 0 6px rgba(0, 0, 0, 0.3)
-  background-color #F5F0F2
-
-::-webkit-scrollbar-thumb
-  box-shadow inset 0 0 6px rgba(0, 0, 0, 0.1)
-  -webkit-box-shadow inset 0 0 6px rgba(0, 0, 0, 0.1)
-  background-color #F5C1BB
-
-::-webkit-scrollbar-thumb:active
-  background-color #FADBD8
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,20 +10,20 @@
 <style lang="stylus" rel="stylesheet/stylus" scoped>
 .card-wrapper
   display inline-block
-  min-width 200px
-  max-width 300px
+  min-width 300px
+  max-width 350px
   margin 10px 5px
   padding 10px
   border-radius 5px
   border 1px solid #F5C1BB
-  background-color #FADBD899
+  background-color rgba(250, 219, 216, 0.6)
   color #303133
   transition box-shadow 0.25s, background-color 0.2s
   user-select none
 
   &:hover
     z-index 9
-    background-color #FADBD8 !important
+    background-color rgba(250, 219, 216, 0.8) !important
     box-shadow 2px 2px 5px rgba(0, 0, 0, 0.1)
 
   .card-text
