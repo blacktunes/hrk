0895c6608147ec51993c8495541769040e3caf7e
blacktunes <blacktunes@foxmail.com>
2021-03-27 00:07:27 +0800

    fix: 修改显示所有模式下卡片文字为可选择

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -19,7 +19,6 @@
   background-color rgba(250, 219, 216, 0.7)
   color #303133
   transition box-shadow 0.25s, background-color 0.2s
-  user-select none
 
   &:hover
     background-color rgba(250, 219, 216, 0.9) !important
